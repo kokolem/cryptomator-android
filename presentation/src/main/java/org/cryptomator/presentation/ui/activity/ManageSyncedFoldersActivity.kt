989c4ceecc3a4ba16063cdba023d8e63fc8f1804
kokolem <vitek.peterk@gmail.com>
2022-04-18 12:26:34 +0200

    Make back arrow work in ManageSyncedFoldersActivity

--- presentation/src/main/java/org/cryptomator/presentation/ui/activity/ManageSyncedFoldersActivity.kt
+++ presentation/src/main/java/org/cryptomator/presentation/ui/activity/ManageSyncedFoldersActivity.kt
@@ -14,9 +14,21 @@ class ManageSyncedFoldersActivity : BaseActivity() {
 	private fun setupToolbar() {
 		toolbar.title = getString(R.string.screen_settings_folder_sync_title)
 		setSupportActionBar(toolbar)
-
-		// FIXME: The back arrow does nothing, same issue as https://github.com/cryptomator/android/issues/438
 		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+	}
+
+
+	override fun onMenuItemSelected(itemId: Int): Boolean = when (itemId) {
+		android.R.id.home -> {
+			// TODO: This is probably the wrong way, but it's the way it's done in AutoUploadChooseVaultActivity
+			//  the right way would be to declare parent activity in manifest
+			//  see: https://github.com/cryptomator/android/commit/84c7d0b907d2587d277c60e8a610b96bc2697015#commitcomment-71513754
+
+			// finish this activity and does not call the onCreate method of the parent activity
+			finish()
+			true
+		}
+		else -> super.onMenuItemSelected(itemId)
 	}
 
 }
